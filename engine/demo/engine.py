221a099a790009fc5930b287ec943f2b90af916a
Huang Peng <shawn.p.huang@gmail.com>
2008-05-29 11:04:25 +0800

    Remove locale panel.

--- engine/demo/engine.py
+++ engine/demo/engine.py
@@ -3,13 +3,11 @@ import gobject
 import gtk
 from ibus import keysyms
 from ibus import interface
-import panel
 
 class Engine (interface.IEngine):
 	def __init__ (self, dbusconn, object_path):
 		interface.IEngine.__init__ (self, dbusconn, object_path)
 		self._dbusconn = dbusconn
-		self._panel = panel.Panel ()
 
 	# methods for dbus rpc
 	def ProcessKeyEvent (self, keyval, is_press, state):
@@ -23,30 +21,22 @@ class Engine (interface.IEngine):
 
 	def FocusIn (self):
 		print "FocusIn"
-		self._panel.focus_in ()
 
 	def FocusOut (self):
 		print "FocusOut"
-		self._panel.focus_out ()
 
 	def Reset (self):
 		print "Reset"
 
 	def SetEnable (self, enable):
 		self._enable = enable
-		self._panel.set_enable (enable)
 
 	def Destroy (self):
 		print "Destroy"
-		self._panel.destroy ()
 
 	def SetCursorLocation (self, x, y, w, h):
-		self._panel.set_cursor_location (x, y, w, h)
+		pass
 
 class DemoEngine (Engine):
 	pass
-
-
-
-
 
