d71e1f65fb0b64613e8700b517611d449e4fb173
Huang Peng <shawn.p.huang@gmail.com>
2008-06-13 23:57:45 +0800

    make panel's methods async.

--- ibusdaemon/panel.py
+++ ibusdaemon/panel.py
@@ -36,35 +36,55 @@ class Panel (ibus.Object):
 		self._ibusconn.connect ("dbus-signal", self._dbus_signal_cb)
 
 	def set_cursor_location (self, x, y, w, h):
-		self._panel.SetCursorLocation (x, y, w, h)
+		self._panel.SetCursorLocation (x, y, w, h,
+				reply_handler = ibus.default_reply_handler,
+				error_handler = ibus.default_error_handler)
 
 	def update_preedit (self, text, attrs, cursor_pos, visible):
-		self._panel.UpdatePreedit (text, attrs, cursor_pos, visible)
+		self._panel.UpdatePreedit (text, attrs, cursor_pos, visible,
+				reply_handler = ibus.default_reply_handler,
+				error_handler = ibus.default_error_handler)
 
 	def update_aux_string (self, text, attrs, visible):
-		self._panel.UpdateAuxString (text, attrs, visible)
+		self._panel.UpdateAuxString (text, attrs, visible,
+				reply_handler = ibus.default_reply_handler,
+				error_handler = ibus.default_error_handler)
 
 	def update_lookup_table (self, lookup_table, visible):
-		self._panel.UpdateLookupTable (lookup_table, visible)
+		self._panel.UpdateLookupTable (lookup_table, visible,
+				reply_handler = ibus.default_reply_handler,
+				error_handler = ibus.default_error_handler)
 
 	def register_properties (self, props):
-		self._panel.RegisterProperties (props)
+		self._panel.RegisterProperties (props,
+				reply_handler = ibus.default_reply_handler,
+				error_handler = ibus.default_error_handler)
 
 	def update_property (self, prop):
-		self._panel.UpdateProperties (prop)
+		self._panel.UpdateProperties (prop,
+				reply_handler = ibus.default_reply_handler,
+				error_handler = ibus.default_error_handler)
 
 	def show_language_bar (self):
-		self._panel.ShowLanguageBar ()
+		self._panel.ShowLanguageBar (
+				reply_handler = ibus.default_reply_handler,
+				error_handler = ibus.default_error_handler)
 
 	def hide_language_bar (self):
-		self._panel.HideLanguageBar ()
+		self._panel.HideLanguageBar (
+				reply_handler = ibus.default_reply_handler,
+				error_handler = ibus.default_error_handler)
 
 	def reset (self):
-		self._panel.Reset ()
+		self._panel.Reset (
+				reply_handler = ibus.default_reply_handler,
+				error_handler = ibus.default_error_handler)
 
 	def destroy (self):
 		if self._ibusconn != None:
-			self._panel.Destroy ()
+			self._panel.Destroy (
+					reply_handler = ibus.default_reply_handler,
+					error_handler = ibus.default_error_handler)
 
 		self._ibusconn = None
 		self._panel = None
