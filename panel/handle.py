253db3e406c4d6d3eead03f0decf08299ff906b9
Huang Peng <shawn.p.huang@gmail.com>
2008-06-18 12:31:33 +0800

    Make languagebar sticks with workarea borders.

--- panel/handle.py
+++ panel/handle.py
@@ -15,32 +15,54 @@ class Handle (gtk.EventBox):
 
 		self._move_begined = False
 
+		root = gdk.get_default_root_window ()
+		workarea = root.property_get ("_NET_WORKAREA")[2]
+
 	def do_button_press_event (self, event):
 		if event.button == 1:
+			root = gdk.get_default_root_window ()
+			desktop = root.property_get ("_NET_CURRENT_DESKTOP")[2][0]
+			self._workarea = root.property_get ("_NET_WORKAREA")[2][desktop * 4: (desktop + 1) * 4]
 			self._move_begined = True
-			self._press_pos = event.x_root, event.y_root
+			toplevel = self.get_toplevel ()
+			x, y = toplevel.get_position ()
+			self._press_pos = event.x_root - x, event.y_root - y
 			self.window.set_cursor (gdk.Cursor (gdk.FLEUR))
-			return True	
+			return True
 		return False
 
 	def do_button_release_event (self, event):
 		if event.button == 1:
 			self._move_begined = False
 			del self._press_pos
+			del self._workarea
 			self.window.set_cursor (gdk.Cursor (gdk.LEFT_PTR))
 			return True
 
 		return False
-		
+
 	def do_motion_notify_event (self, event):
 		if not self._move_begined:
 			return
 		toplevel = self.get_toplevel ()
 		x, y = toplevel.get_position ()
-		x  = int (x + event.x_root - self._press_pos[0])
-		y  = int (y + event.y_root - self._press_pos[1])
+		x  = int (event.x_root - self._press_pos[0])
+		y  = int (event.y_root - self._press_pos[1])
+
+		if x < self._workarea[0] and x > self._workarea[0] - 16:
+			x = self._workarea[0]
+		if y < self._workarea[1] and y > self._workarea[1] - 16:
+			y = self._workarea[1]
+
+		w, h = toplevel.get_size ()
+		if x + w > self._workarea[0] + self._workarea[2] and \
+			x + w < self._workarea[0] + self._workarea[2] + 16:
+			x = self._workarea[0] + self._workarea[2] - w
+		if y + h > self._workarea[1] + self._workarea[3] and \
+			y + h < self._workarea[1] + self._workarea[3] + 16:
+			y =  self._workarea[1] + self._workarea[3] - h
+
 		toplevel.move (x, y)
-		self._press_pos = event.x_root, event.y_root
 
 	def do_expose_event (self, event):
 		self.style.paint_handle (
