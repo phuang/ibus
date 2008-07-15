def0b9a207053c5f206797a6b04d55b44c18ab54
Huang Peng <shawn.p.huang@gmail.com>
2008-07-15 11:37:13 +0800

    Refine coding style of lookuptable.py

--- daemon/lookuptable.py
+++ daemon/lookuptable.py
@@ -21,75 +21,75 @@
 
 import dbus
 
-class Candidates (list):
+class Candidates(list):
 	SIGNATURE = "a(saau)"
-	def to_dbus_value (self):
-		value = dbus.Array (signature = "(saau)")
+	def to_dbus_value(self):
+		value = dbus.Array(signature = "(saau)")
 		for text, attrs in self:
-			value.append ((text, attrs.to_dbus_value ()), "(s%s)" % attrs.SIGNATURE)
+			value.append((text, attrs.to_dbus_value()), "(s%s)" % attrs.SIGNATURE)
 
-	def from_dbus_value (self):
+	def from_dbus_value(self):
 		pass
 
-class LookupTable:
+class LookupTable(object):
 	SIGNATURE = "(ibia(saau))"
 
-	def __init__ (self, page_size = 5):
+	def __init__(self, page_size = 5):
 		self._page_size = page_size
 		self._cursor_visible = False
 		self._cursor_pos = 0
 		self._candidates = []
 
-	def set_page_size (self, page_size):
+	def set_page_size(self, page_size):
 		self._page_size = page_size
 
-	def get_page_size (self):
+	def get_page_size(self):
 		return self._page_size
 
-	def show_cursor (self):
+	def show_cursor(self):
 		self._cursor_visible = True
 
-	def hide_cursor (self):
+	def hide_cursor(self):
 		self._cursor_visible = False
 
-	def is_cursor_visible (self):
+	def is_cursor_visible(self):
 		return self._cursor_visible
 
-	def get_current_page_start (self):
-		return (self._cursor_pos / self._page_size) * self._page_size
+	def get_current_page_start(self):
+		return(self._cursor_pos / self._page_size) * self._page_size
 
-	def set_cursor_pos (self, pos):
+	def set_cursor_pos(self, pos):
 		self._current_pos = pos
 
-	def get_cursor_pos (self):
+	def get_cursor_pos(self):
 		return self._current_pos
 
-	def get_cursor_pos_in_current_page (self):
+	def get_cursor_pos_in_current_page(self):
 		return self._current_pos % self._page_size
 
-	def page_up (self):
+	def page_up(self):
 		pass
 
-	def page_down (self):
+	def page_down(self):
 		pass
 
-	def cursor_up (self):
+	def cursor_up(self):
 		pass
 
-	def cursor_down (self):
+	def cursor_down(self):
 		pass
 
-	def clear (self):
+	def clear(self):
 		self._candidates = []
 
-	def append_candidate (self, candidate, attrs = None):
-		self._candidates.append ((candidates, attrs))
+	def append_candidate(self, candidate, attrs = None):
+		self._candidates.append((candidates, attrs))
 
-	def get_candidate (self, index):
+	def get_candidate(self, index):
 		return self._candidates[index]
 
-	def to_dbus_struct (self):
+	def to_dbus_struct(self):
 		pass
 
-	def from_dbus_struct (self, value):
+	def from_dbus_struct(self, value):
 		pass
