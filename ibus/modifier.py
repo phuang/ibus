627ea998108bd446009e2d61567fbc701c61bc0b
Huang Peng <shawn.p.huang@gmail.com>
2008-07-15 12:59:48 +0800

    Refine coding style of modifier.py

--- ibus/modifier.py
+++ ibus/modifier.py
@@ -19,26 +19,26 @@
 # Free Software Foundation, Inc., 59 Temple Place, Suite 330,
 # Boston, MA  02111-1307  USA
 
-SHIFT_MASK    = 1 << 0
-LOCK_MASK     = 1 << 1
-CONTROL_MASK  = 1 << 2
-ALT_MASK      = 1 << 3
-MOD1_MASK     = 1 << 3
-MOD2_MASK     = 1 << 4
-MOD3_MASK     = 1 << 5
-MOD4_MASK     = 1 << 6
-MOD5_MASK     = 1 << 7
-BUTTON1_MASK  = 1 << 8
-BUTTON2_MASK  = 1 << 9
-BUTTON3_MASK  = 1 << 10
-BUTTON4_MASK  = 1 << 11
-BUTTON5_MASK  = 1 << 12
+SHIFT_MASK = 1 << 0
+LOCK_MASK = 1 << 1
+CONTROL_MASK = 1 << 2
+ALT_MASK = 1 << 3
+MOD1_MASK = 1 << 3
+MOD2_MASK = 1 << 4
+MOD3_MASK = 1 << 5
+MOD4_MASK = 1 << 6
+MOD5_MASK = 1 << 7
+BUTTON1_MASK = 1 << 8
+BUTTON2_MASK = 1 << 9
+BUTTON3_MASK = 1 << 10
+BUTTON4_MASK = 1 << 11
+BUTTON5_MASK = 1 << 12
   
-SUPER_MASK    = 1 << 26
-HYPER_MASK    = 1 << 27
-META_MASK     = 1 << 28
+SUPER_MASK = 1 << 26
+HYPER_MASK = 1 << 27
+META_MASK = 1 << 28
   
-RELEASE_MASK  = 1 << 30
+RELEASE_MASK = 1 << 30
 
 MODIFIER_MASK = 0x5c001fff
 
