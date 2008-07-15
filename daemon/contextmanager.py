0379f83eab1863249f0633383376ec2a8cdbedd6
Huang Peng <shawn.p.huang@gmail.com>
2008-07-15 11:19:15 +0800

    Refine coding style of contextmanager.py

--- daemon/contextmanager.py
+++ daemon/contextmanager.py
@@ -22,23 +22,24 @@
 import ibus
 from inputcontext import InputContext
 
-class ContextManager (ibus.Object):
-	def __init__ (self):
+class ContextManager(ibus.Object):
+	def __init__(self):
+		super(ContextManager, self).__init__()
 		self._contexts = {}
 
-	def create_input_context (self, name, ibusconn):
-		context = InputContext (name, ibusconn)
-		self._contexts[context.get_id ()] = context
-		context.connect ("destroy", self._context_destroy_cb)
+	def create_input_context(self, name, ibusconn):
+		context = InputContext(name, ibusconn)
+		self._contexts[context.get_id()] = context
+		context.connect("destroy", self._context_destroy_cb)
 		return context
 
-	def release_input_context (self, ic, ibusconn):
+	def release_input_context(self, ic, ibusconn):
 		context = self._contexts[ic]
-		context.destroy ()
+		context.destroy()
 
-	def lookup_context (self, ic, ibusconn):
+	def lookup_context(self, ic, ibusconn):
 		return self._contexts[ic]
 
-	def _context_destroy_cb (self, context):
-		del self._contexts[context.get_id ()]
+	def _context_destroy_cb(self, context):
+		del self._contexts[context.get_id()]
 
