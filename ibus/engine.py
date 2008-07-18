ace0f62168e5a53ea08ed35ed6d89597155c3ff9
Huang Peng <shawn.p.huang@gmail.com>
2008-07-18 09:05:16 +0800

    Add EngineBase class.

---
+++ ibus/engine.py
@@ -0,0 +1,157 @@
+# vim:set et sts=4 sw=4:
+#
+# ibus - The Input Bus
+#
+# Copyright (c) 2007-2008 Huang Peng <shawn.p.huang@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA  02111-1307  USA
+
+__all__ = (
+        "EngineBase",
+    )
+
+import ibus
+from ibus import interface
+
+class EngineBase(ibus.Object):
+    def __init__(self, conn, object_path):
+        super(EngineBase, self).__init__()
+        self.__proxy = EngineProxy (self, conn, object_path)
+
+    def process_key_event(self, keyval, is_press, state):
+        return False
+
+    def focus_in(self):
+        pass
+
+    def focus_out(self):
+        pass
+
+    def set_cursor_location(self, x, y, w, h):
+        pass
+
+    def reset(self):
+        pass
+
+    def page_up(self):
+        pass
+
+    def page_down(self):
+        pass
+
+    def cursor_up(self):
+        pass
+
+    def cursor_down(self):
+        pass
+
+    def enable(self):
+        pass
+
+    def disable(self):
+        pass
+
+    def property_activate(self, prop_name, prop_state):
+        pass
+
+    def property_show(self, prop_name):
+        pass
+
+    def property_hide(self, prop_name):
+        pass
+
+    def commit_string(self, text):
+        return self.__proxy.CommitString(text)
+
+    def forward_key_event(self, keyval, is_press, state):
+        return self.__proxy.ForwardKeyEvent(keyval, is_press, state)
+
+    def update_preedit(self, text, attrs, cursor_pos, visible):
+        return self.__proxy.UpdatePreedit(text, attrs.to_dbus_value(), cursor_pos, visible)
+
+    def update_aux_string(self, text, attrs, visible):
+        return self.__proxy.UpdateAuxString(text, attrs.to_dbus_value(), visible)
+
+    def update_lookup_table(self, lookup_table, visible):
+        return self.__proxy.UpdateLookupTable(lookup_table.to_dbus_value(), visible)
+
+    def register_properties(self, props):
+        return self.__proxy.RegisterProperties(self, props.to_dbus_value())
+
+    def update_property(self, prop):
+        return self.__proxy.UpdateProperty(prop.to_dbus_value())
+
+    def do_destroy(self):
+        self.__proxy = None
+        super(EngineBase,self).do_destroy()
+
+
+gobject.type_register(EngineBase)
+
+class EngineProxy(interface.IEngine):
+    def __init__(self, engine, conn, object_path):
+        super(EngineProxy, self).__init__(dbusconn, object_path)
+        self.__conn = conn
+        self.__engine = engine
+
+    def ProcessKeyEvent(self, keyval, is_press, state):
+        return self.__engine.process_key_event(keyval, is_press, state)
+
+    def FocusIn(self):
+        return self.__engine.focus_in()
+
+    def FocusOut(self):
+        return self.__engine.focus_out()
+
+    def SetCursorLocation(self, x, y, w, h):
+        return self.__engine.set_cursor_location(x, y, w, h)
+
+    def Reset(self):
+        return self.__engine.reset()
+
+    def PageUp(self):
+        return self.__engine.page_up()
+
+    def PageDown(self):
+        return self.__engine.page_down()
+
+    def CursorUp(self):
+        return self.__engine.cursor_up()
+
+    def CursorDown(self):
+        return self.__engine.cursor_down()
+
+    def Enable(self, enable):
+        return self.__engine.enable()
+    
+    def Enable(self, enable):
+        return self.__engine.disable()
+
+    def PropertyActivate(self, prop_name, prop_state):
+        return self.__engine.property_activate(prop_name, prop_state)
+
+    def PropertyShow(self, prop_name):
+        return self.__engine.property_show(prop_name)
+    
+    def PropertyHide(self, prop_name):
+        return self.__engine.property_hide(prop_name)
+
+    def Destroy(self):
+        self.__engine.destroy()
+        self.__engine = None
+        self.remove_from_connection ()
+        self.__conn = None
+
--- ibus/interface/iengine.py
+++ ibus/interface/iengine.py
@@ -2,12 +2,12 @@
 #
 # ibus - The Input Bus
 #
-# Copyright (c) 2007-2008 Huang Peng <shawn.p.huang@gmail.com>
+# Copyright(c) 2007-2008 Huang Peng <shawn.p.huang@gmail.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
+# version 2 of the License, or(at your option) any later version.
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -25,85 +25,88 @@ import dbus.service
 from ibus.common import \
     IBUS_ENGINE_IFACE
 
-class IEngine (dbus.service.Object):
+class IEngine(dbus.service.Object):
     # define method decorator.
     method = lambda **args: \
-        dbus.service.method (dbus_interface = IBUS_ENGINE_IFACE, \
+        dbus.service.method(dbus_interface = IBUS_ENGINE_IFACE, \
                             **args)
 
     # define signal decorator.
     signal = lambda **args: \
-        dbus.service.signal (dbus_interface = IBUS_ENGINE_IFACE, \
+        dbus.service.signal(dbus_interface = IBUS_ENGINE_IFACE, \
                             **args)
 
     # define async method decorator.
     async_method = lambda **args: \
-        dbus.service.method (dbus_interface = IBUS_ENGINE_IFACE, \
+        dbus.service.method(dbus_interface = IBUS_ENGINE_IFACE, \
                             async_callbacks = ("reply_cb", "error_cb"), \
                             **args)
 
-    @method (in_signature = "ubu", out_signature = "b")
-    def ProcessKeyEvent (self, keyval, is_press, state):
+    @method(in_signature = "ubu", out_signature = "b")
+    def ProcessKeyEvent(self, keyval, is_press, state):
         pass
 
-    @method (in_signature = "iiii")
-    def SetCursorLocation (self, x, y, w, h): pass
+    @method(in_signature = "iiii")
+    def SetCursorLocation(self, x, y, w, h): pass
 
-    @method ()
-    def FocusIn (self): pass
+    @method()
+    def FocusIn(self): pass
 
-    @method ()
-    def FocusOut (self): pass
+    @method()
+    def FocusOut(self): pass
 
-    @method ()
-    def Reset (self): pass
+    @method()
+    def Reset(self): pass
 
     # signals for lookup table
-    @method ()
-    def PageUp (self): pass
+    @method()
+    def PageUp(self): pass
 
-    @method ()
-    def PageDown (self): pass
+    @method()
+    def PageDown(self): pass
 
-    @method ()
-    def CursorUp (self): pass
+    @method()
+    def CursorUp(self): pass
 
-    @method ()
-    def CursorDown (self): pass
+    @method()
+    def CursorDown(self): pass
 
-    @method (in_signature = "b")
-    def SetEnable (self, enable): pass
+    @method()
+    def Enable(self): pass
+    
+    @method()
+    def Disable(self): pass
 
-    @method (in_signature = "si")
-    def PropertyActivate (self, prop_name, prop_state): pass
+    @method(in_signature = "si")
+    def PropertyActivate(self, prop_name, prop_state): pass
 
-    @method (in_signature = "s")
-    def PropertyShow (self, prop_name): pass
+    @method(in_signature = "s")
+    def PropertyShow(self, prop_name): pass
 
-    @method (in_signature = "s")
-    def PropertyHide (self, prop_name): pass
+    @method(in_signature = "s")
+    def PropertyHide(self, prop_name): pass
 
-    @method ()
-    def Destroy (self): pass
+    @method()
+    def Destroy(self): pass
 
-    @signal (signature="s")
-    def CommitString (self, text): pass
+    @signal(signature="s")
+    def CommitString(self, text): pass
 
-    @signal (signature="ubu")
-    def ForwardKeyEvent (self, keyval, is_press, state): pass
+    @signal(signature="ubu")
+    def ForwardKeyEvent(self, keyval, is_press, state): pass
 
-    @signal (signature="saauib")
-    def UpdatePreedit (self, text, attrs, cursor_pos, visible): pass
+    @signal(signature="saauib")
+    def UpdatePreedit(self, text, attrs, cursor_pos, visible): pass
 
-    @signal (signature="svb")
-    def UpdateAuxString (self, text, attrs, visible): pass
+    @signal(signature="svb")
+    def UpdateAuxString(self, text, attrs, visible): pass
 
-    @signal (signature="vb")
-    def UpdateLookupTable (self, lookup_table, visible): pass
+    @signal(signature="vb")
+    def UpdateLookupTable(self, lookup_table, visible): pass
 
-    @signal (signature="v")
-    def RegisterProperties (self, props): pass
+    @signal(signature="v")
+    def RegisterProperties(self, props): pass
 
-    @signal (signature="v")
-    def UpdateProperty (self, prop): pass
+    @signal(signature="v")
+    def UpdateProperty(self, prop): pass
 
