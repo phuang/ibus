3217d7d3b4dba90f47784729a4306ef6463a2edc
Huang Peng <shawn.p.huang@gmail.com>
2008-07-31 20:14:11 +0800

    Fix typos.

--- gconf/config.py
+++ gconf/config.py
@@ -38,9 +38,9 @@ class Config(ibus.Object):
             (gobject.TYPE_STRING, gobject.TYPE_PYOBJECT)),
     }
 
-    def __init__ (self, _ibus = None, path = None):
+    def __init__ (self, bus = None, path = None):
         super(Config, self).__init__()
-        conn = _ibus.get_conn() if _ibus != None else None
+        conn = bus.get_dbusconn() if bus != None else None
         self.__proxy = ConfigProxy(self, conn, path)
         self.__client = gconf.Client()
         self.__handler_id = self.__client.connect("value-changed", self.__value_changed_cb)
