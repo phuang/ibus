2be1293b9da7f52910969e4a7de04f1fa6b14f6b
Huang Peng <shawn.p.huang@gmail.com>
2008-09-09 15:37:06 +0800

    Add -Wl,--no-undefined in CFLAGS.

--- autogen.sh
+++ autogen.sh
@@ -8,6 +8,6 @@ aclocal -I m4
 autoheader
 automake --add-missing --copy
 autoconf
-export CFLAGS="-Wall -g -O0"
+export CFLAGS="-Wall -g -O0 -Wl,--no-undefined"
 export CXXFLAGS="$CFLAGS"
 ./configure --enable-maintainer-mode $*
