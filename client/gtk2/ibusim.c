1ca5958dd3afa1241eb88cc93873d1d5ddbd8824
Huang Peng <shawn.p.huang@gmail.com>
2008-09-17 10:42:59 +0800

    Use g_fprintf

--- client/gtk2/ibusim.c
+++ client/gtk2/ibusim.c
@@ -17,10 +17,10 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
+
+#include <glib/gprintf.h>
 #include <gtk/gtk.h>
 #include <gtk/gtkimmodule.h>
-#include <string.h>
-#include <stdio.h>
 #include "ibusimclient.h"
 #include "ibusimcontext.h"
 
