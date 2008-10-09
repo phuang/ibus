bf989c52300810ff5e9ac9e864782013ab583ee9
Huang Peng <shawn.p.huang@gmail.com>
2008-10-09 16:19:28 +0800

    Add src folder for c language codes.

---
+++ src/ibusobject.c
@@ -0,0 +1,111 @@
+/* vim:set et ts=4: */
+/* ibus - The Input Bus
+ * Copyright (C) 2008-2009 Huang Peng <shawn.p.huang@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "ibusobject.h"
+#include "ibusinternel.h"
+
+enum {
+    DESTROY,
+    LAST_SIGNAL,
+};
+
+
+/* IBusObjectPriv */
+struct _IBusObjectPrivate {
+};
+
+static guint            _signals[LAST_SIGNAL] = { 0 };
+
+/* functions prototype */
+static void     ibus_object_class_init      (IBusObjectClass  *klass);
+static void     ibus_object_init            (IBusObject       *client);
+static void     ibus_object_finalize        (GObject            *obj);
+
+static GObjectClass *_parent_class = NULL;
+
+
+GType
+ibus_object_get_type (void)
+{
+    static GType type = 0;
+    
+    static const GTypeInfo type_info = {
+        sizeof (IBusObjectClass),
+        (GBaseInitFunc)     NULL,
+        (GBaseFinalizeFunc) NULL,
+        (GClassInitFunc)    ibus_object_class_init,
+        NULL,               /* class finialize */
+        NULL,               /* class data */
+        sizeof (IBusObject),
+        0,
+        (GInstanceInitFunc) ibus_object_init,
+    };
+
+    if (type == 0) {
+        type = g_type_register_static (G_TYPE_OBJECT,
+                    "IBusObject",
+                    &type_info,
+                    (GTypeFlags)0);
+    }
+    
+    return type;
+}
+
+IBusObject *
+ibus_object_new (void)
+{
+    return IBUS_OBJECT (g_object_new (IBUS_TYPE_OBJECT, NULL));
+}
+
+static void
+ibus_object_class_init     (IBusObjectClass *klass)
+{
+    GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+
+    _parent_class = (GObjectClass *) g_type_class_peek_parent (klass);
+
+    g_type_class_add_private (klass, sizeof (IBusObjectPrivate));
+
+    gobject_class->finalize = ibus_object_finalize;
+
+    _signals[DESTROY] =
+        g_signal_new (I_("destroy"),
+            G_TYPE_FROM_CLASS (gobject_class),
+            G_SIGNAL_RUN_FIRST,
+            G_STRUCT_OFFSET (IBusObjectClass, destroy),
+            NULL, NULL,
+            ibus_marshal_VOID__VOID,
+            G_TYPE_NONE, 0);
+}
+
+static void
+ibus_object_init (IBusObject *obj)
+{
+}
+
+
+static void
+ibus_object_finalize (GObject *obj)
+{
+    g_signal_emit (obj, _signals[DESTROY], 0);
+    G_OBJECT_CLASS(_parent_class)->finalize (obj);
+}
+
+
---
+++ src/ibusobject.h
@@ -0,0 +1,84 @@
+/* vim:set et ts=4: */
+/* ibus - The Input Bus
+ * Copyright (C) 2008-2009 Huang Peng <shawn.p.huang@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+#ifndef __IBUS_OBJECT_H_
+#define __IBUS_OBJECT_H_
+
+#include <glib-object.h>
+#include "ibusmarshalers.h"
+
+/*
+ * Type macros.
+ */
+
+/* define GOBJECT macros */
+#define IBUS_TYPE_OBJECT             \
+    (ibus_object_get_type ())
+#define IBUS_OBJECT(obj)             \
+    (G_TYPE_CHECK_INSTANCE_CAST ((obj), IBUS_TYPE_OBJECT, IBusObject))
+#define IBUS_OBJECT_CLASS(klass)     \
+    (G_TYPE_CHECK_CLASS_CAST ((klass), IBUS_TYPE_OBJECT, IBusObjectClass))
+#define IBUS_IS_OBJECT(obj)          \
+    (G_TYPE_CHECK_INSTANCE_TYPE ((obj), IBUS_TYPE_OBJECT))
+#define IBUS_IS_OBJECT_CLASS(klass)  \
+    (G_TYPE_CHECK_CLASS_TYPE ((klass), IBUS_TYPE_OBJECT))
+#define IBUS_OBJECT_GET_CLASS(obj)   \
+    (G_TYPE_CHECK_GET_CLASS ((obj), IBUS_TYPE_OBJECT, IBusObjectClass))
+
+#if 0
+#define DEBUG_FUNCTION_IN   g_debug("%s IN", __FUNCTION__);
+#define DEBUG_FUNCTION_OUT  g_debug("%s OUT", __FUNCTION__);
+#else
+#define DEBUG_FUNCTION_IN
+#define DEBUG_FUNCTION_OUT
+#endif
+
+G_BEGIN_DECLS
+
+enum IBusCap {
+    IBUS_CAP_PREEDIT = (1),
+    IBUS_CAP_AUX_STRING = (1 << 1),
+    IBUS_CAP_LOOKUP_TABLE = (1 << 2),
+    IBUS_CAP_FOCUS = (1 << 3),
+};
+
+typedef struct _IBusObject IBusObject;
+typedef struct _IBusObjectClass IBusObjectClass;
+typedef struct _IBusObjectPrivate IBusObjectPrivate;
+
+struct _IBusObject {
+  GObject parent;
+  /* instance members */
+  IBusObjectPrivate *priv;
+};
+
+struct _IBusObjectClass {
+  GObjectClass parent;
+
+  /* class members */
+  void (* destroy)        (IBusObject   *client);
+};
+
+GType           ibus_object_get_type            (void);
+IBusObject     *ibus_object_new                 (void);
+void           *ibus_object_destroy             (IBusObject     *object);
+
+G_END_DECLS
+#endif
+
