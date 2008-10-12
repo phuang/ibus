e45d73534b01372dbf63505fa8b831aa8ea8a24e
Huang Peng <shawn.p.huang@gmail.com>
2008-10-12 20:10:39 +0800

    WIP.

---
+++ bus/main.c
@@ -0,0 +1,30 @@
+#include <ibus.h>
+
+
+void connection_destroy_cb (IBusConnection *connection, gpointer user_data)
+{
+	g_debug ("connnection %p destroyed", connection );
+}
+
+void new_connection_cb (IBusServer *server, IBusConnection *connection, gpointer user_data)
+{
+	g_debug ("new-connection %p", connection);
+	g_signal_connect (connection, "destroy", connection_destroy_cb, 0);
+}
+
+int main()
+{
+	g_type_init ();
+	
+	GMainLoop *mainloop;
+	IBusConnection *connection;
+	IBusServer *server;
+	
+	mainloop = g_main_loop_new (NULL, FALSE);
+	server = ibus_server_listen ("unix:abstract=/tmp/1234567");
+	g_signal_connect (server, "new-connection", new_connection_cb, 0);
+	
+	g_main_loop_run (mainloop);
+	
+	return 0;
+}
