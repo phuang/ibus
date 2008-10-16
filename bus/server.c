a73c283b508c38b3cb980759d450cbc355d2d126
Huang Peng <shawn.p.huang@gmail.com>
2008-10-16 15:56:21 +0800

    Remove unused code.

--- bus/server.c
+++ bus/server.c
@@ -123,67 +123,6 @@ bus_server_init (BusServer *server)
     priv->dbus_impl = bus_ibus_impl_new ();
 }
 
-
-
-#if 0
-static gboolean
-_connection_dbus_message_cb     (BusConnection  *connection,
-                                 DBusMessage    *message,
-                                 BusServer      *server)
-{
-    g_assert (BUS_IS_CONNECTION (connection));
-    g_assert (BUS_IS_SERVER (server));
-    g_assert (message != NULL);
-
-    gint i;
-    gboolean retval = FALSE;
-
-    struct {
-        const gchar *interface;
-        const gchar *name;
-        gboolean (* handler) (BusServer *, DBusMessage *, BusConnection *);
-    } handlers[] =  {
-        /* ibus interface */
-        { IBUS_INTERFACE_IBUS, "GetAddress",            _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "CreateInputContext",    _ibus_create_input_context },
-        { IBUS_INTERFACE_IBUS, "ReleaseInputContext",   _ibus_release_input_context },
-        { IBUS_INTERFACE_IBUS, "ProcessKeyEvent",       _ibus_process_key_event },
-        { IBUS_INTERFACE_IBUS, "SetCursorLocation",     _ibus_set_cursor_location },
-        { IBUS_INTERFACE_IBUS, "FocusIn",               _ibus_focus_in },
-        { IBUS_INTERFACE_IBUS, "FocusOut",              _ibus_focus_out },
-        { IBUS_INTERFACE_IBUS, "Reset",                 _ibus_reset },
-        { IBUS_INTERFACE_IBUS, "GetIsEnabled",          _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "SetCapabilites",        _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "RegisterFactories",     _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "UnregisterFactories",   _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "GetFactoryInfo",        _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "SetFactory",            _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "GetInputContextStates", _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "RegisterListEngines",   _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "RegisterReloadEngines", _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "RegisterStartEngine",   _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "RegisterRestartEngine", _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "RegisterStopEngine",    _ibus_get_address },
-        { IBUS_INTERFACE_IBUS, "Kill",                  _ibus_get_address },
- 
-        {NULL, NULL, NULL},
-    };
-
-    while (handlers[i].interface != NULL ) {
-        if (dbus_message_is_method_call (message,
-                                         handlers[i].interface,
-                                         handlers[i].name)) {
-            retval = handlers[i].handler (server, message, connection);
-            g_signal_stop_emission_by_name (connection, "dbus-message");
-            return retval;
-        }
-
-    }
-    return retval;
-}
-#endif
-
-
 static void
 bus_server_new_connection   (BusServer          *server,
                              BusConnection      *connection)
