aaa9a8cea3a23bca6444221192ffc52ccbff3cb8
Huang Peng <shawn.p.huang@gmail.com>
2009-02-05 10:48:58 +0800

    Add gconf/config.h

---
+++ gconf/config.h
@@ -0,0 +1,28 @@
+/* vim:set et sts=4: */
+#ifndef __CONFIG_GCONF_H__
+#define __CONFIG_GCONF_H__
+
+#include <ibus.h>
+#include <gconf/gconf-client.h>
+
+#define IBUS_TYPE_CONFIG_GCONF	\
+	(ibus_config_gconf_get_type ())
+
+typedef struct _IBusConfigGConf IBusConfigGConf;
+typedef struct _IBusConfigGConfClass IBusConfigGConfClass;
+
+struct _IBusConfigGConf {
+	IBusConfigService parent;
+    GConfClient *client;
+};
+
+struct _IBusConfigGConfClass {
+	IBusConfigServiceClass parent;
+
+};
+
+
+GType            ibus_config_gconf_get_type     (void);
+IBusConfigGConf *ibus_config_gconf_new          (IBusConnection     *connection);
+
+#endif
