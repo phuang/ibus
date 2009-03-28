56760794ebe6bb23e2fe2fdc5e3dca825be54df7
Peng Huang <shawn.p.huang@gmail.com>
2009-03-28 12:12:47 +0800

    exit 1 when some command failed.

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,13 @@
 set -e
 set -x
 
-autopoint  --force
-libtoolize --automake --copy --force
-gtkdocize  --copy #--flavour=no-tmpl
-aclocal -I m4 --force
-autoheader --force
-automake --add-missing --copy --force
-autoconf --force
+autopoint  --force || exit 1
+libtoolize --automake --copy --force || exit 1
+gtkdocize  --copy || exit 1 #--flavour=no-tmpl
+aclocal -I m4 --force || exit 1
+autoheader --force || exit 1
+automake --add-missing --copy --force || exit 1
+autoconf --force || exit 1
 export CFLAGS="-Wall -g -O0 -Wl,--no-undefined"
 export CXXFLAGS="$CFLAGS"
-./configure --enable-maintainer-mode $*
+./configure --enable-maintainer-mode $* || exit 1
