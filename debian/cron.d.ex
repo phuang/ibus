fd1fe90c0d71f5e721efbf69a6630542afc9176c
Peng Huang <phuang@phuang-laptop.(none)>
2009-09-30 13:36:19 +0800

    Update debian packaging scripts from ubuntu

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ibus package
-#
-0 4	* * *	root	ibus_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ibus):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ibus" command="/usr/bin/ibus"
