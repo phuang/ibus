36a7755099d3888105b1a156b82e3c40e66410fc
Peng Huang <shawn.p.huang@gmail.com>
2009-12-10 16:30:36 +0800

    Use correct GType to replace gint

--- src/ibusconfigprivate.h
+++ src/ibusconfigprivate.h
@@ -28,7 +28,7 @@ _from_dbus_value (IBusMessageIter   *iter,
     g_assert (iter != NULL);
     g_assert (value != NULL);
 
-    gint type;
+    GType type;
     IBusMessageIter sub_iter;
 
     type = ibus_message_iter_get_arg_type (iter);
@@ -83,7 +83,7 @@ _from_dbus_value (IBusMessageIter   *iter,
         if (type == IBUS_TYPE_ARRAY) {
             GValue v = { 0 };
             IBusMessageIter sub_iter;
-            gint sub_type;
+            GType sub_type;
             GValueArray *array;
 
 
