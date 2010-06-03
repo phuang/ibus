efad0c3e731da7a3ca46a68d04ffb60614752fa5
Peng Huang <shawn.p.huang@gmail.com>
2010-06-03 08:43:08 +0800

    Add a small test code for gir

---
+++ src/python/ibus.py
@@ -0,0 +1,11 @@
+from gi.repository.IBus import \
+    Bus, \
+    Engine, \
+    Attribute, \
+    AttrList, \
+    Text, \
+    LookupTable, \
+    Property, \
+    PropList, \
+    Component, \
+    EngineDesc
