1b06b1f9b1ae304d82a5658a6e2dfb92b51da481
Peng Huang <shawn.p.huang@gmail.com>
2010-06-06 11:14:42 +0800

    Remove python in bindings

--- bindings/python/common.h
+++
@@ -1,7 +0,0 @@
-#ifndef __PY_COMMON_H_
-#define __PY_COMMON_H_
-
-#include <pygobject.h>
-#include <ibus.h>
-
-#endif //__PY_COMMON_H_
--- bindings/python/gendefs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-codegendir=`pkg-config pygobject-2.0 --variable=codegendir`
-python $codegendir/h2def.py -m ibus ../../src/*.h
