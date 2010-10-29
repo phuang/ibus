d00fb1d1ce308f4848ede89ca4ff3cd3994923d6
Peng Huang <shawn.p.huang@gmail.com>
2010-10-29 14:25:50 +0900

    Add i18n.py.

---
+++ ui/gtk/i18n.py
@@ -0,0 +1,32 @@
+# vim:set et sts=4 sw=4:
+#
+# ibus - The Input Bus
+#
+# Copyright(c) 2007-2010 Peng Huang <shawn.p.huang@gmail.com>
+# Copyright(c) 2007-2010 Google, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or(at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA  02111-1307  USA
+
+import gettext
+import os
+
+_ = lambda a: gettext.dgettext("ibus10", a)
+N_ = lambda a: a
+
+def init():
+    localedir = os.getenv("IBUS_LOCALEDIR")
+    gettext.bindtextdomain("ibus10", localedir)
+    gettext.bind_textdomain_codeset("ibus10", "UTF-8")
