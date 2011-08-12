6ec33ea8cdf16c6225896351a59ac9a01d5e36ca
Peng Huang <shawn.p.huang@gmail.com>
2011-08-11 20:23:18 -0400

    Add missing config.h in gdk-private.c
    Forget include config.h in gdk-private.c. So the HAVE_X11_XKBLIB_H is always undefined, and ibux-x11 can not get correct group from x key event.
    
    BUG=ibus-x11 can not handle group correctly
    TEST=Linux desktop
    
    Review URL: http://codereview.appspot.com/4865041

--- client/x11/gdk-private.c
+++ client/x11/gdk-private.c
@@ -20,6 +20,9 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
+
+#include "config.h"
+
 #include <gtk/gtk.h>
 #include <gdk/gdkx.h>
 #include <gdk/gdkkeysyms.h>
