fc72d2f6b11595e38216fbffd1048e2ba07d524d
Peng Huang <shawn.p.huang@gmail.com>
2012-01-05 17:34:30 -0500

    refine code for reviewing

--- engine/gensimple.py
+++ engine/gensimple.py
@@ -1,5 +1,24 @@
 # vim:set et sts=4 sw=4:
-#!/usr/bin/env python
+#
+# ibus - The Input Bus
+#
+# Copyright (c) 2007-2010 Peng Huang <shawn.p.huang@gmail.com>
+# Copyright (c) 2007-2010 Red Hat, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA  02111-1307  USA
 
 from xml.dom import minidom
 import cgi
