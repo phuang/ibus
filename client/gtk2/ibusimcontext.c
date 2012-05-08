7ca7db00f8d8feb5d68873089ac3418c795a0c15
Jason Conti <jason.conti@gmail.com>
2012-05-08 15:42:38 -0400

    Use ibus_bus_new_async in gtk2 client.
    BUG=http://code.google.com/p/ibus/issues/detail?id=1452
    TEST=Manually
    
    Review URL: https://codereview.appspot.com/6198052
    Patch from Jason Conti <jason.conti@gmail.com>.

--- client/gtk2/ibusimcontext.c
+++ client/gtk2/ibusimcontext.c
@@ -502,7 +502,7 @@ ibus_im_context_class_init (IBusIMContextClass *class)
     /* init bus object */
     if (_bus == NULL) {
         ibus_set_display (gdk_display_get_name (gdk_display_get_default ()));
-        _bus = ibus_bus_new ();
+        _bus = ibus_bus_new_async ();
 
         /* init the global fake context */
         if (ibus_bus_is_connected (_bus)) {
