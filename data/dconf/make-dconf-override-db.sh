4bee2863e8d2e51b185b6d149e6259848d8c0cb6
Daiki Ueno <ueno@unixuser.org>
2012-05-20 15:01:01 +0900

    dconf: use text-based key file and dconf update
    BUG=none
    TEST=manually
    
    Review URL: https://codereview.appspot.com/6215062

--- data/dconf/make-dconf-override-db.sh
+++ data/dconf/make-dconf-override-db.sh
@@ -15,17 +15,32 @@ trap 'rm -rf $TMPDIR; kill $DBUS_SESSION_BUS_PID' ERR
 # in case that schema is not installed on the system
 glib-compile-schemas --targetdir "$GSETTINGS_SCHEMA_DIR" "$PWD"
 
-gsettings list-recursively org.freedesktop.ibus.general | \
-while read schema key val; do
-    gsettings set "$schema" "$key" "$val"
+cat <<EOF
+# This file is a part of the IBus packaging and should not be changed.
+#
+# Instead create your own file next to it with a higher numbered prefix,
+# and run
+#
+#       dconf update
+#
+EOF
+
+# Loop over top level schemas since "gsettings list-recursively" only
+# looks for direct children.
+schemas="org.freedesktop.ibus.general org.freedesktop.ibus.panel"
+current_schema=
+for schema in $schemas; do
+  gsettings list-recursively $schema | \
+  while read schema key val; do
+    if test "$schema" != "$current_schema"; then
+      echo
+      echo $schema | sed 's/org\.freedesktop\(.*\)/[desktop\1]/' | tr '.' '/'
+      current_schema="$schema"
+    fi
+    echo "$key=$val"
+  done
 done
 
-gsettings list-recursively org.freedesktop.ibus.panel | \
-while read schema key val; do
-    gsettings set "$schema" "$key" "$val"
-done
-
-mv $XDG_CONFIG_HOME/dconf/user "$1"
 rm -rf $TMPDIR
 
 kill $DBUS_SESSION_BUS_PID
