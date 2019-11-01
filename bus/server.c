efeb7b9689126e4f05bd1e10441cdc4a3720ff1c
Robert Hoffmann <externer.dl.hoffmann@muenchen.de>
2019-11-01 21:55:13 +0900

    bus: Fix restart crash with inotify read()
    Do not close inotify desciptors when restarting the server. This
    may lead to crashes in glib.
    
    Fixes crash in libglib/libgio with
    GLib-GIO-ERROR : inotify read(): Bad file descriptor
    
    See also src/tests/ibus-bus.c: start_exit_async():
        /* When `./runtest ibus-bus` runs, ibus-daemon sometimes failed to
         * restart because closing a file descriptor was failed in
         * bus/server.c:_restart_server() with a following error:
         *     "inotify read(): Bad file descriptor"
    
    Test plan:
    Run
    $ ibus restart
    several times. Without the patch ibus-daemon will crash occasionally.
    
    BUG=https://github.com/ibus/ibus/pull/2148

--- bus/server.c
+++ bus/server.c
@@ -26,6 +26,8 @@
 #include <glib/gstdio.h>
 #include <gio/gio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <string.h>
 
 #include "dbusimpl.h"
@@ -45,16 +47,33 @@ _restart_server (void)
 {
     gchar *exe;
     gint fd;
+    ssize_t r;
+    int MAXSIZE = 0xFFF;
+    char proclnk[MAXSIZE];
+    char filename[MAXSIZE];
 
-    exe = g_strdup_printf ("/proc/%d/exe", getpid ());
-    exe = g_file_read_link (exe, NULL);
+    exe = g_file_read_link ("/proc/self/exe", NULL);
 
     if (exe == NULL)
-        exe = BINDIR "/ibus-daemon";
+        exe = g_strdup (BINDIR "/ibus-daemon");
 
     /* close all fds except stdin, stdout, stderr */
     for (fd = 3; fd <= sysconf (_SC_OPEN_MAX); fd ++) {
-        close (fd);
+        errno = 0;
+        /* only close valid fds */
+        if (fcntl (fd, F_GETFD) != -1 || errno != EBADF) {
+            g_sprintf (proclnk, "/proc/self/fd/%d", fd);
+            r = readlink (proclnk, filename, MAXSIZE);
+            if (r < 0) {
+                continue;
+            }
+            filename[r] = '\0';
+
+            /* Do not close 'anon_inode:inotify' fds, that may crash in glib */
+            if (g_strcmp0 (filename, "anon_inode:inotify") != 0) {
+                close (fd);
+            }
+        }
     }
 
     _restart = FALSE;
@@ -69,6 +88,7 @@ _restart_server (void)
         execv (exe, g_argv);
     }
     g_warning ("execv %s failed!", g_argv[0]);
+    g_free (exe);
     exit (-1);
 }
 
