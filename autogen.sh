2bc47f09832073c87ccdf1060bd93ff95878685a
fujiwarat <takao.fujiwara1@gmail.com>
2021-12-02 23:52:36 +0900

    autogen: Delete gnome-autogen dependency
    You may like to call ./autogen.sh --enable-gtk-doc with your custom
    configure options.
    Also set SAVE_DIST_FILE=1 by default

--- autogen.sh
+++ autogen.sh
@@ -1,22 +1,47 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+: ${srcdir=$(dirname $0)}
+: ${srcdir:=.}
+: ${SAVE_DIST_FILES:=0}
 
-PKG_NAME="ibus"
-DIST_FILES="
-engine/simple.xml.in
-src/ibusemojigen.h
-src/ibusunicodegen.h
-"
+olddir=$(pwd)
+# shellcheck disable=SC2016
+PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
+WANT_GTK_DOC=0
+GCC_VERSION=$(gcc --version | head -1 | awk '{print $3}')
+GCC_MAJOR_VERSION=$(echo "$GCC_VERSION" | awk -F. '{print $1}')
 FEDORA_PKG1='autoconf automake libtool gettext-devel'
 FEDORA_PKG2='glib2-devel gtk2-devel gtk3-devel
  wayland-devel'
 FEDORA_PKG3='cldr-emoji-annotation iso-codes-devel unicode-emoji unicode-ucd
  xkeyboard-config-devel'
 
-(test -z "$DISABLE_INSTALL_PKGS") && {
+CFLAGS=${CFLAGS-"-Wall -Wformat -Werror=format-security"}
+(test $GCC_MAJOR_VERSION -ge 10) && {
+    CFLAGS="$CFLAGS -fanalyzer -fsanitize=address -fsanitize=leak"
+    FEDORA_PKG1="$FEDORA_PKG1 libasan"
+}
+
+cd "$srcdir"
+
+(test -f configure.ac \
+  && test -f README ) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+(test $(grep -q "^GTK_DOC_CHECK" configure.ac)) || {
+    WANT_GTK_DOC=1
+    FEDORA_PKG2="$FEDORA_PKG2 gtk-doc"
+}
+
+(test -f ChangeLog) || {
+    touch ChangeLog
+}
+
+(test "x$DISABLE_INSTALL_PKGS" = "x") && {
     (test -f /etc/fedora-release ) && {
         rpm -q $FEDORA_PKG1 || exit 1
         rpm -q $FEDORA_PKG2 || exit 1
@@ -27,30 +52,39 @@ FEDORA_PKG3='cldr-emoji-annotation iso-codes-devel unicode-emoji unicode-ucd
     }
 }
 
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/README ) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
+CONFIGFLAGS="$@"
+(test "$#" = 0 -a "x$NOCONFIGURE" = "x" ) && {
+    echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
+    echo "*** If you wish to pass any to it, please specify them on the" >&2
+    echo "*** '$0' command line." >&2
+    echo "" >&2
+    (test $WANT_GTK_DOC -eq 1) && CONFIGFLAGS="--enable-gtk-doc $@"
 }
 
-which gnome-autogen.sh || {
-    echo "Not found gnome-autogen.sh. You may need to install gnome-common"
-    exit 1
+(test $WANT_GTK_DOC -eq 1) && gtkdocize --copy
+
+ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I m4" REQUIRED_AUTOMAKE_VERSION=1.11 \
+autoreconf --verbose --force --install || exit 1
+
+cd "$olddir"
+(test "x$NOCONFIGURE" = "x" ) && {
+    echo "$srcdir/configure $CONFIGFLAGS"
+    $srcdir/configure $CONFIGFLAGS || exit 1
+    (test "$1" = "--help" ) && {
+        exit 0
+    } || {
+        echo "Now type 'make' to compile $PKG_NAME" || exit 1
+    }
+} || {
+    echo "Skipping configure process."
 }
 
-(test -f $srcdir/ChangeLog) || {
-    touch $srcdir/ChangeLog
-}
-
-CFLAGS=${CFLAGS-"-Wall -Wformat -Werror=format-security"}
-
-# need --enable-gtk-doc for gnome-autogen.sh to make dist
-ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I m4" REQUIRED_AUTOMAKE_VERSION=1.11 CFLAGS="$CFLAGS" . gnome-autogen.sh "$@"
-
-(test -z "$SAVE_DIST_FILES") && {
-    for f in $DIST_FILES ; do
-        echo "rm $f"
-        rm $f
-    done
+cd "$srcdir"
+(test "x$SAVE_DIST_FILES" = "x0" ) && {
+    # rm engine/simple.xml.in src/ibusemojigen.h src/ibusunicodegen.h
+    for d in engine src; do
+        echo "make -C $d maintainer-clean-generic"
+        make -C $d maintainer-clean-generic
+   done
 } || :
+cd "$olddir"
